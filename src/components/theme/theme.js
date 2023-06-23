8613b15fd0986b1caac2f3c5a1d568688ed4afa7
nicolasgodineau <nicolasgodineau@live.fr>
2023-06-22 22:54:57 -0400

    Modification du thème

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -2,31 +2,17 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
     palette: {
-        primary: {
-            main: "#0b1847",
-        },
-
-        secondary: {
-            main: "#4c66dc",
-        },
-
-        light: {
-            main: "#ffffff",
-        },
-
         background: {
             dark: "#1f1f1f",
             light: "#f9f9f9",
         },
 
         text: {
-            primary: "#fffff",
+            primary: "#fff",
             secondary: "#999999",
-            links: "#185ce3",
+            accent: "#28e98c",
         },
-        accent: {
-            main: "#28e98c",
-        },
+        accent: "#28e98c",
     },
     breakpoints: {
         values: {
@@ -35,34 +21,6 @@ const theme = createTheme({
             md: 900,
             lg: 1200,
             xl: 1536,
-        },
-    },
-
-    typography: {
-        //https://mui.com/material-ui/customization/typography/
-        fontFamily: "Roboto, sans-serif", // Default font family
-
-        h1: {
-            fontFamily: "Roboto Flex, sans-serif", // Font family for h1 variant
-        },
-        h2: {
-            fontFamily: "Inter, sans-serif", // Font family for h2 variant
-        },
-        h4: {
-            fontFamily: "Inter, sans-serif", // Font family for h2 variant
-        },
-        heading: {
-            fontFamily: "Times, serif", // Font family for heading variant
-        },
-        subtitle1: {
-            fontFamily: "Helvetica", // Font family for subtitle1 variant
-        },
-        body: {
-            fontFamily: "Inter, sans-serif", // Font family for body variant
-        },
-        button: {
-            // fontStyle: 'italic',
-            fontFamily: "Roboto Flex, sans-serif", // Font family for body variant
         },
     },
 });
