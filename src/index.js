0c6171fa8be7bec8c27b7eaa404f7d79ae1b9f8b
nicolasgodineau <nicolasgodineau@live.fr>
2023-07-13 12:19:09 -0400

    clean code et modifications du manifest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom";
-import "./index.css";
+import "../src/index.css";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import "aos/dist/aos.css";
