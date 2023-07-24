ca1d55dde6785da37dc18dc0fe5284b860a78682
nicolasgodineau <nicolasgodineau@live.fr>
2023-07-24 11:33:33 -0400

    Ajout de la détection de langue dans l'index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import "aos/dist/aos.css";
 
 import App from "./App";
@@ -10,21 +11,22 @@ import translationEN from "./lang/en.json";
 import translationFR from "./lang/fr.json";
 
 // Configurer i18next
-i18n.use(initReactI18next).init({
-    resources: {
-        en: {
-            translation: translationEN,
+i18n.use(LanguageDetector) // Utilise le détecteur de langue du navigateur
+    .use(initReactI18next)
+    .init({
+        resources: {
+            en: {
+                translation: translationEN,
+            },
+            fr: {
+                translation: translationFR,
+            },
         },
-        fr: {
-            translation: translationFR,
+        fallbackLng: "en", // Langue de secours si la langue détectée n'est pas prise en charge
+        interpolation: {
+            escapeValue: false,
         },
-    },
-    lng: "en",
-    fallbackLng: "en",
-    interpolation: {
-        escapeValue: false,
-    },
-});
+    });
 
 // Rendre l'application en utilisant createRoot de ReactDOM
 const root = createRoot(document.getElementById("root"));
